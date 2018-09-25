d42ba11526cd5f59709db87a2eb7c5e7353496f5
BreezeDawn <BreezeGent@gmail.com>
2018-09-25 20:46:02 +0800

    用户中心-获取用户详细信息(优化)

--- Store/Store/apps/users/views.py
+++ Store/Store/apps/users/views.py
@@ -1,11 +1,9 @@
 # Create your views here.
 from rest_framework import status
-from rest_framework.generics import CreateAPIView, GenericAPIView
+from rest_framework.generics import CreateAPIView, GenericAPIView, RetrieveAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
-
-
 
 from users.models import User
 from users.serializers import CreateUserSerializer, UserDetailSerializer
@@ -18,21 +16,15 @@ class UsersTestView(APIView):
         return Response({"api": 'users'}, status=status.HTTP_200_OK)
 
 
-class UserDetailView(GenericAPIView):
+class UserDetailView(RetrieveAPIView):
+    """使用RetrieveAPIView获取当前用户详细信息"""
     # 指定当前视图所用的权限控制类
     permission_classes = [IsAuthenticated]
     serializer_class = UserDetailSerializer
 
+    # 改写get_object方法返回当前用户
     def get_object(self):
         return self.request.user
-
-    def get(self,request):
-        # 获取用户
-        user = self.get_object()
-        # 序列化用户信息并返回
-        serializer = self.get_serializer(user)
-        return Response(serializer.data)
-
 
 
 class UsersCountView(APIView):
